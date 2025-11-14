b2831a2260264f1adb262f52ed1c72102a985ae7
GTGowtham <129858473+GTGowtham@users.noreply.github.com>
2025-11-15 01:37:06 +0530

    Update weather scheduler and DB scripts

--- scripts/weather_api_db.py
+++ scripts/weather_api_db.py
@@ -4,8 +4,10 @@ import pandas as pd
 import os 
 import sys
 import pymysql
-import sqlalchemy
+from sqlalchemy import create_engine,Column,String,Integer,Float
+from sqlalchemy.orm import declarative_base,sessionmaker
 from dotenv import load_dotenv
+from sqlalchemy import DateTime,func
 load_dotenv()
 
 api_key=os.getenv("weather_api")
@@ -43,12 +45,49 @@ def get_weather_data(city: str):
         "description": description
     }
 
+base=declarative_base()
+
+class weather(base):
+    __tablename__="weather_data"
+    timestamp=Column(DateTime,server_default=func.now())
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    city=Column(String(50))
+    temperature=Column(Float)
+    humidity=Column(Integer)
+    description=Column(String(50))
+
+db_user=os.getenv("DB_USER")
+db_password=os.getenv("DB_PASSWORD")
+db_host=os.getenv("DB_HOST")
+db_port=os.getenv("DB_PORT")
+db_name=os.getenv("weather_db_name")
+
+db_url=f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+engine=create_engine(db_url)
+base.metadata.create_all(engine)
+
+
+session=sessionmaker(bind=engine)
+session=session()
+print("successfully create tables")
+
+
+
 if __name__ == "__main__":
     result = get_weather_data(city)
     if result:
-        print(f"\nCity: {result['city']}")
-        print(f"Temperature: {result['temperature']} °C")
-        print(f"Humidity: {result['humidity']}%")
-        print(f"Weather: {result['description']}")
+        new_record = weather(
+        city=result["city"],
+        temperature=result["temperature"],
+        humidity=result["humidity"],
+        description=result["description"]
+    )
+    
+    session.add(new_record)
+    session.commit()
+    print("weather data inserted successfully")
+
+
 
       
---
+++ scripts/weather_scheduler.py
@@ -0,0 +1,28 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from scripts.weather_api_db import get_weather_data, session, weather, city
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def save_weather():
+    data = get_weather_data(city)
+    if data:
+        record = weather(
+            city=data["city"],
+            temperature=data["temperature"],
+            humidity=data["humidity"],
+            description=data["description"]
+        )
+        session.add(record)
+        session.commit()
+        print("Weather data inserted.")
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+
+    scheduler.add_job(save_weather, 'cron', hour='7,13,20', minute=0)
+    scheduler.add_job(save_weather, 'interval', minutes=1)
+
+    print("Scheduler started. Running at 7 AM, 1 PM, and 8 PM.")
+    scheduler.start()
